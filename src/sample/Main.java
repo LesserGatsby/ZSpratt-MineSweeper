5464bd0ec381e4c2f2ca710a86d2f2b443acfdd7
Zachary Spratt <Zachman331@gmail.com>
2016-04-14 14:03:23 -0700

    Made base UI setup

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -1,19 +1,28 @@
 package sample;
 
 import javafx.application.Application;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.ChoiceBox;
 import javafx.stage.Stage;
 
 public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("MineSweeperUI.fxml"));
         primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
+        primaryStage.setScene(new Scene(root, 400, 600));
         primaryStage.show();
+
+        ChoiceBox choiceBox = (ChoiceBox) root.lookup("#DifficultyBox");
+        System.out.println(choiceBox);
+        ObservableList<String> options = FXCollections.observableArrayList("Easy", "Medium", "Hard");
+        choiceBox.setItems(options);
+        choiceBox.getSelectionModel().selectFirst();
     }
 
 
